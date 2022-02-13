81b2e39cbe16f00d769bbc06168de04a27a64ae8
Aditya Chache <84438200+adityachache@users.noreply.github.com>
2022-02-13 19:10:50 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 
+# URL OF THE PRODUCT
 PRODUCT_URL = "https://www.amazon.in/Gigabyte-GeForce-2060-GDDR6-Graphics/dp/B08CRZTTBZ/ref=sr_1_1?crid=NUT888REIBLB&keywords=rtx+2060&qid=1643278179&sprefix=rtx+2060%2Caps%2C275&sr=8-1"
+
 HEADERS = {
     "Accept-Language": "en-US,en;q=0.9",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
